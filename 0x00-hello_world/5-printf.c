e9c4228a11eede1dbe5d5c4f6feb7b704f90a522
Qasmeer <casmirluke1397@gmail.com>
2023-09-07 18:28:16 -0700

    hello world update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line using printf function
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
