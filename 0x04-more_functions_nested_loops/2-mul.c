2f7f5c39b5f4f2276dab46569b3c6ba905fded83
Qasmeer <casmirluke1397@gmail.com>
2023-09-14 20:18:07 -0700

    more functions update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - func multiplies two integers
+ * @a: a - var
+ * @b: b - var
+ * Return: always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
